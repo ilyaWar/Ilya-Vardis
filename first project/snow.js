c29a2caa4e0eebdf3bf89daf763784e007ebc8a9
ilyaWar <ilya-vardis@mail.ru>
2022-06-21 20:00:18 +0300

    1

---
+++ first project/snow.js
@@ -0,0 +1,15 @@
+var snowboard = [
+	{
+		umgProduct:" images/боты.png",
+		nameProduct:"Nightshade Snowboard",
+		sellPrise: "$ 398.93 $499.00",
+	}
+]
+var div = document.querySelecotr();
+var htmlStr = ''
+for(var i=0; i<snowboard.length;i++){
+	htmlStr += `
+	
+	
+	`
+}
